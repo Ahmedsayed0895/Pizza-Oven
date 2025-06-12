4589ea8adf939e5d3c1fb833b0fd59bd53caf3e7
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-06-12 07:06:14 +0300

    feat: Add list of toppings drawables
    This commit introduces a new Kotlin file, `toppingsList.kt`, which defines a list named `toppingList`. This list contains drawable resource IDs for five different topping images (basil_8, onion_3, broccoli_3, mushroom_3, and sausage_3).

---
+++ app/src/main/java/com/example/pizzaoven/data/toppingsList.kt
@@ -0,0 +1,10 @@
+package com.example.pizzaoven.data
+
+import com.example.pizzaoven.R
+
+val toppingList = listOf(
+    R.drawable.basil_8,
+    R.drawable.onion_3,
+    R.drawable.broccoli_3,
+    R.drawable.mushroom_3,
+    R.drawable.sausage_3,)
