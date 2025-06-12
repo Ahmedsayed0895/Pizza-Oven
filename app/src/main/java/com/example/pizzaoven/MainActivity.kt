3014d44efb01efa0558c1fddcc9e624be2a03b4f
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-06-12 07:09:31 +0300

    Fix: Correct padding in "Add to cart" button
    The padding of the `Row` within the "Add to cart" `Button` has been corrected.
    The `vertical` padding was previously missing and has been set to `8.dp`.

--- app/src/main/java/com/example/pizzaoven/MainActivity.kt
+++ app/src/main/java/com/example/pizzaoven/MainActivity.kt
@@ -5,38 +5,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
-import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -48,7 +38,6 @@ import com.example.pizzaoven.data.toppingList
 import com.example.pizzaoven.ui.theme.PizzaOvenTheme
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalMaterial3Api::class)
     @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -183,7 +172,7 @@ class MainActivity : ComponentActivity() {
                                 shape = RoundedCornerShape(12.dp),
                                 modifier = Modifier.padding(16.dp)
                             ) {
-                                Row (modifier = Modifier.padding(horizontal = 24.dp, vertical = 8.dp),){
+                                Row (modifier = Modifier.padding(horizontal = 24.dp, vertical = 8.dp)){
                                     Icon(
                                         painter = painterResource(R.drawable.cart_shopping),
                                         contentDescription = "cart",
