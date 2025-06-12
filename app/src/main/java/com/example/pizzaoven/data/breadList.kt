40bd3e4823440781fe0fd4d004e56b5bc2286979
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-06-12 07:06:04 +0300

    feat: Add list of bread drawables
    This commit introduces a new Kotlin file, `breadList.kt`, which defines a list named `breadList`. This list contains drawable resource IDs for five different bread images (bread_1, bread_2, bread_3, bread_4, and bread_5).

---
+++ app/src/main/java/com/example/pizzaoven/data/breadList.kt
@@ -0,0 +1,11 @@
+package com.example.pizzaoven.data
+
+import com.example.pizzaoven.R
+
+val breadList = listOf(
+    R.drawable.bread_1,
+    R.drawable.bread_2,
+    R.drawable.bread_3,
+    R.drawable.bread_4,
+    R.drawable.bread_5,
+)
